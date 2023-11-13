c4726baf188a87043befb6acd53a7883cad59cf3
AxelXavierOlivarLozano <149702848+AxelXavierOlivarLozano@users.noreply.github.com>
2023-11-13 09:45:24 -0600

    Update sumaParesPromedio.cpp

--- sumaParesPromedio.cpp
+++ sumaParesPromedio.cpp
@@ -12,33 +12,34 @@ using namespace std;
 // Función que sume todos los números enteros pares entre el 0 y el 100.
 int sumaPares( ){
  //1.  Declararación de variables
-int suma = 0;
-
-    for (int i = 0; i <= 100; i += 2) {
-        suma += i;
+    int sumaPares = 0;
+    for(int i = 0; i<= 100;){
+        sumaPares = sumaPares+i;
+        i=i+2;
     }
-
   // retorna la suma de todos los pares
-    return suma ;
+    return sumaPares;
 }
 
 
 // Función que calcula el promedio todos los números enteros pares entre el 0 y el 100.
 int promedioPares( ){
   // 1.  Declararación de variables
-   int suma = sumaPares();
-    int cantidadPares = 51; 
-    int promedio = suma / cantidadPares;
-
+    double suma = 0;
+    int contador = 0;
+    for(int i = 0; i<=100; i += 2){
+        suma += i;
+        contador++;
+    }
   // retorna el promedio
-    return promedio ;
+    return suma/contador;
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    
-    cout << "Suma de todos los números pares entre 0 y 100" << sumaPares( ) << endl;
-    cout <<"Promedio de todos los números pares entre 0 y 100:" << promedioPares( ) << endl;
-    
+
+    cout << sumaPares( ) << endl;
+    cout << promedioPares( ) << endl;
+
     return 0;
 }
